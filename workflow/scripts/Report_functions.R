0dbde9a1bb9f8ee32cb8649feb98f3684f82e803
LuisaArias <ariasg.luisam@gmail.com>
2025-11-03 11:26:22 +0100

    Removed unnecessary code from Report_functions.R, Added further description to plots in Orochi report

--- workflow/scripts/Report_functions.R
+++ workflow/scripts/Report_functions.R
@@ -19,17 +19,14 @@ library(yaml)
 metaphlan_secondary <- params$metaphlan_secondary
 config_file <- params$config_file
 
-#config_file <- here("config", "configfile.yaml") # change to the relative corresponding path
 config <- yaml::read_yaml(config_file)
 
-# merged_table.txt (output from rule MetaPhlAn_secondary)
-#metaphlan_secondary <- snakemake@input[["metaphlan_secondary"]]
 # samples.tsv FROM CONFIG
 samples <- config$samples
+
 # outdir FROM CONFIG
 outdir <- config$outdir
-# dir where \\.emapper\\.annotations\\.adjusted\\.tsv is (line 423)
-# ...
+
 
 plotsdir <- "results/08_plots/PLOTS/1-Reads"
 plotsdirbins <- "results/08_plots/PLOTS/3-Bins"
@@ -42,18 +39,10 @@ rsc_path <- file.path(outdir, "results/08_plots/rsc")
 
 # Making object -----------------------------------------------------------
 #(https://github.com/YuLab-SMU/MicrobiotaProcess/issues/58)
-#mpse <- mp_import_metaphlan(profile='./merged_table.txt') ## CHANGE output from rule MetaPhlAn_secondary
 
 ## TO CONNECT TO SNAKEMAKE
 # Access input file
 mpse <- mp_import_metaphlan(profile = metaphlan_secondary)
-
-# Including pools information
-#sample_groups <- read.table("../orochiNIOO/config/inUse_samples.tsv", header = TRUE) ## CHANGE
-#colnames(sample_groups)[1] <- "Sample"
-#sample_groups <- sample_groups[,c(1,length(sample_groups))]
-#mpse3 <- mpse %>%
-#  left_join(sample_groups, by = "Sample")
 
 # Including TREATMENT information (in my case, treatment1)
 sample_groups <- read.table(samples, header = TRUE)
@@ -141,61 +130,6 @@ plot2_taxonomic_abundance <- function(level = "Family") {
   taxa_plots[[level]]
 }
 
-## MAKING ONE BAR PER POOL
-# onebar_taxa_plots <- list()
-# 
-# for (level in taxa_levels) {
-#   # Collapse to mean abundance per Family per treatment
-#   collapsed_family <- mpse3 %>%
-#     group_by(!!sym(level), treatment1, Sample) %>%
-#     summarise(taxon_abund = sum(RelAbundanceBySample), .groups = "drop") %>%
-#     group_by(!!sym(level), treatment1) %>%
-#     summarise(mean_abund = mean(taxon_abund), .groups = "drop")
-# 
-#   # Reshape: wide format (rows = Family, cols = treatment, values = abundance)
-#   mat <- collapsed_family %>%
-#     pivot_wider(names_from = treatment1, values_from = mean_abund, values_fill = 0) %>%
-#     as.data.frame()
-#   rownames(mat) <- mat[,1]
-#   mat[,1] <- NULL
-# 
-#   # Step 2: build MPSE object correctly
-#   mpse_treatment <- MPSE(
-#     assays = list(Abundance = as.matrix(mat))
-#   )
-# 
-#   # Step 3: plot with mp_plot_abundance
-#   p3 <- mpse_treatment %>%
-#     mp_plot_abundance(
-#       .abundance = Abundance,
-#       taxa.class = level,
-#       relative = FALSE,
-#       force = TRUE,
-#       topn = 20
-#     )
-#   p3 <- p3 + labs(title = paste("(Bacterial) Taxonomic Abundance at", level)) +
-#     theme(plot.title = element_text(hjust = 0.5))
-#   
-#   # Save each plot with its level in filename
-#   ggplot2::ggsave(
-#     filename = paste0("plots/2-taxonomic_abundance_average_", tolower(level), ".tiff"),
-#     plot = p3,
-#     width = 12,
-#     height = 10,
-#     units = "in",
-#     dpi = 500,
-#     compression = "lzw"
-#   )
-#   onebar_taxa_plots[[level]] <- p3
-# }
-# 
-# plot2_average_taxonomic_abundance <- function(level = "Family") {
-#   if (!level %in% names(onebar_taxa_plots)) {
-#     stop("Invalid level! Choose from: ", paste(names(onebar_taxa_plots), collapse = ", "))
-#   }
-#   onebar_taxa_plots[[level]]
-# }
-
 ## As heatmaps
 
 taxa_plots_heatmap <- list()
@@ -207,7 +141,6 @@ for (level in taxa_levels) {
   h1 <- mpse3 %>%
     mp_plot_abundance(
       .abundance = Abundance,
-      #.group = sample_pool,
       .group = treatment1,
       taxa.class = !!sym(level),
       relative = TRUE,
@@ -296,8 +229,6 @@ for (level in taxa_levels) {
   h2 <- mpse_treatment %>%
     mp_plot_abundance(
       .abundance = Abundance,
-      #.group = sample_pool,
-      #.group = treatment1,
       taxa.class = !!sym(level),
       relative = TRUE,
       topn = 20,
@@ -377,15 +308,13 @@ mpse3 %<>% mp_cal_pcoa(.abundance=hellinger, distmethod="bray", .dim = 2)
 #    show.legend = FALSE # don't display the legend of stat_ellipse
 #  )
 
-# The size of point also can be mapped to other variables such as Observe, or Shannon 
-# Then the alpha diversity and beta diversity will be displayed simultaneously.
 pcoa2 <- mpse3 %>% 
   mp_plot_ord(
     .ord = pcoa, 
     .group = treatment1, 
     .color = treatment1, 
-    .size = 4, #Observe, 
-    .alpha = 0.8, #Shannon,
+    .size = 4,
+    .alpha = 0.8,
     ellipse = TRUE,
     show.legend = FALSE # don't display the legend of stat_ellipse 
   ) +
